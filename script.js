331901019ed3d9cbcb578852ff409ea6a974fd73
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-11-14 11:58:51 -0500

    part C complete

--- script.js
+++ script.js
@@ -35,3 +35,27 @@ ex: Hello, my name is Jean Valjean*/
 
 let myName = "Tasnim Chowdhury";
 console.log(`Hello, my name is ${myName}`)
+
+/* C. Booleans
+Using the provided variable definitions, replace the blanks so that all log statements print true in the console.
+Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console*/
+ 
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' == 'Name'); 
+// FOR THE NEXT TWO, USE ONLY && OR || 
+console.log(true || false); 
+console.log(false || false || false || false || false || true); 
+
+console.log(false == false) 
+console.log(e === 'Kevin'); 
+console.log(a < b && b < c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations) 
+console.log(a * a == d); // note: the answer is a simple arithmetic equation, not something "weird"
+console.log(48 == '48');
+
